d60b2090734fca969c760ee665a162ffeab739b8
SebastianS-1997 <125471975+SebastianS-1997@users.noreply.github.com>
2023-06-21 17:44:28 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Define the path to the scripts
+script1_path = r"your/path\mtDNA_analysis/Scripts\Step_ALL_VCF.py"
+
+# Run each script using the subprocess module
+subprocess.run(["python", script1_path]) 
